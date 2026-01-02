b41e898ff1a9c4c040380eee444f0d7d8ef8e39b
IfeelLikeSnow <ifeellikesnow@googlemail.com>
2026-01-02 22:55:32 +0100

    go

--- Scripts/IFLS_IDM_Toolbar/DF95/Installers/IFLS_Print_Action_Script_Path.lua
+++
@@ -1,7 +0,0 @@
--- @description IFLS: Print current script path (diagnostics)
--- @version 1.0
--- @author IFLS
--- @about Prints the absolute path of THIS script instance. Helpful when multiple duplicates exist in Action List.
-local _, script_path = reaper.get_action_context()
-reaper.ShowConsoleMsg("This script path: "..tostring(script_path).."\n")
-reaper.ShowMessageBox("This script path:\n\n"..tostring(script_path), "IFLS Diagnostics", 0)
